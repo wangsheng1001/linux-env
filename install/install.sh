adc2de0d3d70e0b1dd09ff1425caf421e9b6a55c
WANG Sheng <wangsheng1001@gmail.com>
2017-05-02 06:24:29 -0700

    Add curl in apt-get install

--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,4 @@
-sudo apt-get install vim
-sudo apt-get install default-jdk
-sudo apt-get install exuberant-ctags
+sudo apt-get install -y vim
+sudo apt-get install -y default-jdk
+sudo apt-get install -y exuberant-ctags
+sudo apt-get install -y curl
