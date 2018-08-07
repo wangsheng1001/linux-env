533a0ba21c1d1121fdcc71ee6a32a1a19940abbf
WANG Sheng <wangsheng1001@gmail.com>
2018-08-07 15:58:43 +0800

    Add vim CtrlP Plugin

--- vim/install-plugins.sh
+++ vim/install-plugins.sh
@@ -8,6 +8,9 @@ cd ~/.vim/bundle && git clone https://github.com/scrooloose/nerdtree.git
 # install TagBar
 cd ~/.vim/bundle && git clone git://github.com/majutsushi/tagbar
 
+# install CtrlP
+cd ~/.vim/bundle && git clone https://github.com/ctrlpvim/ctrlp.vim.git
+
 # install YCM
 cd ~/.vim/bundle && git clone https://github.com/Valloric/YouCompleteMe.git
 cd YouCompleteMe && git submodule update --init --recursive
