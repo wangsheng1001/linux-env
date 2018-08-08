a12ec2c809e35e91b2345ef259bb27a827431fb9
WANG Sheng <wangsheng1001@gmail.com>
2018-08-08 16:59:48 +0800

    Refine ycm config file

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -1,9 +1,6 @@
-import os
 import os.path
-import fnmatch
 import logging
 import ycm_core
-import re
 
 BASE_FLAGS = [
         '-Wall',
@@ -19,72 +16,35 @@ BASE_FLAGS = [
         '-I/usr/include/',
         ]
 
-SOURCE_EXTENSIONS = [
-        '.cpp',
-        '.cxx',
-        '.cc',
-        '.c',
-        '.m',
-        '.mm'
-        ]
-
-SOURCE_DIRECTORIES = [
-        'src',
-        'lib'
-        ]
-
-HEADER_EXTENSIONS = [
-        '.h',
-        '.hxx',
-        '.hpp',
-        '.hh'
-        ]
-
 HEADER_DIRECTORIES = [
         'include'
         ]
 
-BUILD_DIRECTORY = 'build';
-
-def FindNearest(path, target, build_folder=None):
+def FindNearest(path, target):
     # check if target dir exists in the path
     candidate = os.path.join(path, target)
-    if(os.path.isfile(candidate) or os.path.isdir(candidate)):
+    if (os.path.isfile(candidate) or os.path.isdir(candidate)):
         logging.info("Found nearest " + target + " at " + candidate)
-        return candidate;
-
+        return candidate
     # check if the path has parent dir
-    parent = os.path.dirname(os.path.abspath(path));
-    if(parent == path):
-        raise RuntimeError("Could not find " + target);
-
-    if(build_folder):
-        candidate = os.path.join(parent, build_folder, target)
-        if(os.path.isfile(candidate) or os.path.isdir(candidate)):
-            logging.info("Found nearest " + target + " in build folder at " + candidate)
-            return candidate;
-
-    return FindNearest(parent, target, build_folder)
+    parent = os.path.dirname(os.path.abspath(path))
+    if (parent == path):
+        return None
+    return FindNearest(parent, target)
 
 def FlagsForInclude(root):
     # find all possible locations containing project header files
-    try:
-        flags = []
-        for header_dir in HEADER_DIRECTORIES:
-            include_path = FindNearest(root, header_dir)
-            if include_path:
-                flags = flags + ["-I" + include_path]
-        return flags
-    except:
-        return None
+    flags = []
+    for header_dir in HEADER_DIRECTORIES:
+        include_path = FindNearest(root, header_dir)
+        if include_path:
+            flags += ["-I" + include_path]
+    return flags
 
 def FlagsForFile(filename):
-    root = os.path.realpath(filename);
-    final_flags = BASE_FLAGS
-    include_flags = FlagsForInclude(root)
-    if include_flags:
-        final_flags = final_flags + include_flags
+    root = os.path.realpath(filename)
+    flags = BASE_FLAGS + FlagsForInclude(root)
     return {
-            'flags': final_flags,
+            'flags': flags,
             'do_cache': True
             }
