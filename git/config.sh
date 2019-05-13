0525cc18633b20a4ca9d80f4441e3cf1a430439b
WANG Sheng <wangsheng1001@gmail.com>
2019-05-13 17:08:19 +0800

    Add colors for git commands

--- git/config.sh
+++ git/config.sh
@@ -3,6 +3,7 @@
 git config --global user.name "WANG Sheng"
 git config --global user.email wangsheng1001@gmail.com
 git config --global core.editor vim
+git config --global color.ui auto
 
 # list configurations
 git config --list
