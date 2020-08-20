62f6b65d5fd101164391cbd23dd1658569c88f4e
WANG Sheng <wangsheng1001@gmail.com>
2020-08-21 02:18:54 +0800

    Add gcc clang compilers

--- install/install.sh
+++ install/install.sh
@@ -3,6 +3,8 @@
 COMMAND=apt-get
 # COMMAND=yum
 
+sudo $COMMAND install devtoolset-7 llvm-toolset-7
+sudo $COMMAND install llvm-toolset-7-clang-analyzer llvm-toolset-7-clang-tools-extra # optional
 sudo $COMMAND install -y cmake
 sudo $COMMAND install -y vim astyle
 sudo $COMMAND install -y default-jdk
